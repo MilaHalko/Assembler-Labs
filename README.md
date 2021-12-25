569da833e3d6ed8d0207e6029e51ae4c04d9d462
Mila Halko <milagalko1@gmail.com>
2021-12-25 15:22:43 +0200

    get x*9 programm

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Assembler-Labs
+Write number (-32768;32767) (3640 is max to get result x * 9): $'
--- task.asm
+++ task.asm
@@ -1,38 +1,140 @@
-STSEG SEGMENT PARA STACK "STACK"
-DB 64 DUP ("STACK")
-STSEG ENDS
+SSG SEGMENT PARA STACK 'STACK'
+    DB 64 DUP (?)
+SSG ENDS
 
-DSEG SEGMENT PARA PUBLIC "DATA"
-SOURCE DB 10, 20, 30, 40
-DEST DB 4 DUP ("?")
-DSEG ENDS
+DSG SEGMENT PARA PUBLIC 'DATA'
+    BUFFER DB 7, 0, 7 DUP(?)
+    NUM DW ?
+    ISNEG DB 0
+    ISERROR DB 0
+    TEN DW 10
 
-CSEG SEGMENT PARA PUBLIC "CODE"
-MAIN PROC FAR
-ASSUME CS: CSEG,DS:DSEG,SS:STSEG
+    MSG_INPUT DB 'Write number (-32768;32767) (3640 is max to get result x * 9): $'
+    MSG_ERR   DB 'Wrong number!$'
+DSG ENDS
 
-PUSH DS
-MOV AX,0
-PUSH AX
+CSG SEGMENT PARA PUBLIC "CODE"
+    MAIN PROC FAR
+        ASSUME CS:CSG, DS:DSG, SS:SSG
+        PUSH DS
+        XOR AX, AX
+        PUSH AX
+        MOV AX, DSG
+        MOV DS, AX
+            
+        CALL INPUT
+        CALL CHECKNUM
+        CALL CALCULATE
+        CALL OUTPUT
+        RET
+    MAIN ENDP
+    
+    INPUT PROC NEAR
+        MOV AH, 9
+        LEA DX, MSG_INPUT
+        INT 21H
 
-MOV AX,DSEG
-MOV DS,AX
+        MOV AH, 10
+        LEA DX, BUFFER
+        INT 21H
+        RET
+    INPUT ENDP
+    
+    CHECKNUM PROC NEAR 
+        XOR AX, AX
+        LEA SI, BUFFER+1
+        MOV CL, [SI]
+        INC SI
+        
+        XOR BX, BX
+        MOV BL, [SI]
+        CMP BL, '-'
+        JNE CHECK_SYMBOL
+        INC SI
+        MOV ISNEG, 1
+        DEC CX
 
-MOV DEST,0
-MOV DEST+1,0
-MOV DEST+2,0
-MOV DEST+3,0
+        CHECK_SYMBOL:
+            IMUL TEN
+            JO ERROR
 
-MOV AL,SOURCE
-MOV DEST+3,AL
-MOV AL,SOURCE+1
-MOV DEST+2,AL
-MOV AL,SOURCE+2
-MOV DEST+1,AL
-MOV AL,SOURCE+3
-MOV DEST,AL
+            MOV BL, [SI]
+            CMP BL, '0'
+            JB ERROR
+            CMP BL, '9'
+            JA ERROR
+            
+            SUB BL, '0'
+            ADD AX, BX
+            JNC INC_FOR_NEXT
+            CMP ISNEG, 0
+            JE ERROR
+            CMP AX, 32768
+            JE ERROR
 
-RET
-MAIN ENDP
-CSEG ENDS
+            INC_FOR_NEXT:
+                INC SI
+                LOOP CHECK_SYMBOL
+        
+        CMP ISNEG, 0
+        JE END_CHECK
+        NEG AX
+        END_CHECK:
+            MOV NUM, AX
+        RET
+    CHECKNUM ENDP
+
+    CALCULATE PROC NEAR     ;NUM*9
+        MOV AX, NUM
+        MOV BX, 9
+        IMUL BX
+        JO ERROR
+        MOV NUM, AX
+        RET
+    CALCULATE ENDP
+
+    OUTPUT PROC NEAR 
+        CALL NEXTLINE
+        CMP ISNEG, 0
+        JE SET_BASIC
+        MOV AL, '-'
+        INT 29H
+        NEG NUM
+
+        SET_BASIC:
+            MOV AX, NUM
+            XOR CX, CX
+        STACKING:
+            XOR DX, DX
+            DIV TEN 
+            ADD DL, '0'     ;'8' -> SCII CODE
+            PUSH DX
+            INC CX
+            TEST AX, AX
+            JNZ STACKING
+        UNSTACKING:
+            POP AX
+            INT 29H
+            LOOP UNSTACKING
+        RET
+    OUTPUT ENDP
+    
+    NEXTLINE PROC NEAR
+        MOV AH, 2
+        MOV DL, 10          ; ASCII [10]/[AH] - NEW LINE
+        INT 21H
+        RET
+    NEXTLINE ENDP
+    
+    ERROR PROC NEAR
+        CALL NEXTLINE
+        MOV AH, 9
+        LEA DX, MSG_ERR
+        INT 21H
+        MOV AX, 4C00H       ;ЗАВЕРШЕНИЕ ПРОГИ
+        INT 21H
+        RET
+    ERROR ENDP
+
+CSG ENDS
 END MAIN
